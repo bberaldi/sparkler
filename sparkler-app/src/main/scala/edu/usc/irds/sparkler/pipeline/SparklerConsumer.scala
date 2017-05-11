98846a7b0b94ce2c8027455b005a01eea81cd63a
Beraldi <bberaldi@gdb.net>
2017-05-11 11:38:13 -0300

    add kafka consumer

---
+++ sparkler-app/src/main/scala/edu/usc/irds/sparkler/pipeline/SparklerConsumer.scala
@@ -0,0 +1,54 @@
+package edu.usc.irds.sparkler.pipeline
+
+import java.util.concurrent.{ExecutorService, Executors}
+import java.util.{Collections, Properties}
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+
+/**
+  * Created by beraldi on 10/05/17.
+  */
+// scalastyle:off
+class SparklerConsumer {
+
+  val TOPIC="csv"
+
+  val  props = new Properties()
+  props.put("bootstrap.servers", "192.168.1.119:9092")
+
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "something")
+
+  val consumer = new KafkaConsumer[String, String](props)
+  var executor: ExecutorService = null
+
+  def putProperty(key: String, value: String): Unit ={
+    props.put(key, value)
+  }
+
+  def shutdown() = {
+    if (consumer != null)
+      consumer.close();
+    if (executor != null)
+      executor.shutdown();
+  }
+
+  def run() = {
+    consumer.subscribe(Collections.singletonList(TOPIC))
+
+    Executors.newSingleThreadExecutor.execute(new Runnable {
+      override def run(): Unit = {
+        while (true) {
+          val records = consumer.poll(1000)
+
+          for (record <- records) {
+            println("Received message: (" + record + ")")
+          }
+        }
+      }
+    })
+  }
+
+
+  }
